315cdc420f2a12cd4094606a923eaba15c4a3004
Daniel Quezada <dquezada478@gmail.com>
2024-09-24 08:52:51 -0700

    Add Lambda update workflow

---
+++ configuration-service/configuration-lambda.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("UserConfiguration")
+
+
+def lambda_handler(event, context):
+    body = json.loads(event["body"])
+    user_id = body["UserID"]
+    s3_bucket = body["S3BucketName"]
+    jq_expression = body["JQExpression"]
+
+    try:
+        table.put_item(
+            Item={
+                "UserID": user_id,
+                "S3BucketName": s3_bucket,
+                "JQExpression": jq_expression,
+            }
+        )
+        return {"statusCode": 200, "body": json.dumps("Configuration saved!")}
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps(f"Error saving configuration: {str(e)}"),
+        }
