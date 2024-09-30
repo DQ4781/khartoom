278e34aaa0af247eadc88867418889858981a334
Daniel Quezada <dquezada478@gmail.com>
2024-09-29 21:44:03 -0700

    Mock DynamoDB in tests using moto

--- configuration-service/test_configuration_service.py
+++ configuration-service/test_configuration_service.py
@@ -1,13 +1,26 @@
 import os
 import json
 from configuration import lambda_handler
+from moto import mock_dynamodb2
 
 
 os.environ["AWS_REGION"] = "us-west-2"
 os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
 
 
+@mock_dynamodb2
 def test_save_configuration():
+    # Create a mock DynamoDB instance
+    dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
+
+    # Create a mock table (it won't persist but will exist for the test)
+    dynamodb.create_table(
+        TableName="UserConfiguration",
+        KeySchema=[{"AttributeName": "UserID", "KeyType": "HASH"}],
+        AttributeDefinitions=[{"AttributeName": "UserID", "AttributeType": "S"}],
+        ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+    )
+
     event = {
         "body": json.dumps(
             {
