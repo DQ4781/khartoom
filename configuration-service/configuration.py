a9dcd2a96d1f114e281f7126da2c11060f67bbd3
Daniel Quezada <dquezada478@gmail.com>
2024-11-07 09:52:41 -0800

    Add: V2 ingest & config src code

--- configuration-service/configuration.py
+++ configuration-service/configuration.py
@@ -1,29 +1,62 @@
+########
+#  V2 Configuration
+#########
 import os
 import json
 import boto3
 
 dynamodb = boto3.resource("dynamodb", region_name=os.getenv("AWS_REGION", "us-west-2"))
-table = dynamodb.Table("UserConfiguration")
+user_config_table = dynamodb.Table("UserConfiguration")
+api_key_table = dynamodb.Table("UserAPIKeyTable")
 
 
 def lambda_handler(event, context):
     try:
         body = json.loads(event["body"])
-        user_id = body["UserID"]
-        s3_bucket = body["S3BucketARN"]
-        jq_expression = body["JQExpression"]
+        email = body.get("Email")
+        api_key = body.get("APIKey")
+        s3_bucket = body.get("S3BucketARN")
+        jq_expression = body.get("JQExpression")
 
-        table.put_item(
+        if not email or not api_key or not s3_bucket or not jq_expression:
+            return {
+                "statusCode": 400,
+                "body": json.dumps(f"Missing required configuration fields."),
+            }
+    except (json.JSONDecodeError, KeyError) as e:
+        return {
+            "statusCode": 400,
+            "body": json.dumps(f"Invalid request format: {str(e)}"),
+        }
+
+    # Validate the API key
+    if not validate_api_key(api_key, email):
+        return {"statusCode": 403, "body": json.dumps(f"Invalid API key.")}
+
+    # Store configuration
+    try:
+        user_config_table.put_item(
             Item={
-                "UserID": user_id,
+                "Email": email,
                 "S3BucketARN": s3_bucket,
                 "JQExpression": jq_expression,
             }
         )
         return {"statusCode": 200, "body": json.dumps("Configuration saved!")}
     except Exception as e:
-        print(f"Error: {str(e)}")
+        print(f"Error putting configuration for email {email}: {str(e)}")
         return {
             "statusCode": 500,
             "body": json.dumps(f"Error saving configuration: {str(e)}"),
         }
+
+
+def validate_api_key(api_key, email):
+    try:
+        response = api_key_table.get_item(Key={"Email": email})
+        if "Item" in response and response["Item"].get("APIKey") == api_key:
+            return True
+        return False
+    except Exception as e:
+        print(f"Error validating API key: {e}")
+        return False
--- ingestion-service/ingestion.py
+++ ingestion-service/ingestion.py
@@ -1,8 +1,13 @@
+########
+#  V2 Ingestion
+#########
 import json
 import boto3
 
 dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("UserConfiguration")
+user_config_table = dynamodb.Table("UserConfiguration")
+api_key_table = dynamodb.Table("UserAPIKeyTable")
+
 
 # Initialize AWS Lambda client
 lambda_client = boto3.client("lambda")
@@ -11,28 +16,51 @@ lambda_client = boto3.client("lambda")
 def lambda_handler(event, context):
     try:
         body = json.loads(event["body"])
-        user_id = body["UserID"]
-        data = body["data"]
+        email = body.get("Email")
+        api_key = body.get("APIKey")
+        data = body.get("Data")
+
+        if not email or not api_key or not data:
+            return {
+                "statusCode": 400,
+                "body": json.dumps(f"Missing required ingestion fields."),
+            }
     except (json.JSONDecodeError, KeyError) as e:
         return {"statusCode": 400, "body": json.dumps(f"Invalid request: {str(e)}")}
 
+    # Validate the API key
+    if not validate_api_key(api_key, email):
+        return {"statusCode": 403, "body": json.dumps(f"Invalid API key.")}
+
     # Fetch configuration from our DynamoDB table
     try:
-        response = table.get_item(Key={"UserID": user_id})
+        response = user_config_table.get_item(Key={"Email": email})
         if "Item" not in response:
             return {
                 "statusCode": 404,
                 "body": json.dumps("User configuration not found"),
             }
 
-        config = response["Item"]
-        s3_bucket = config["S3BucketARN"]
-        jq_expression = config["JQExpression"]
+        try:
+            config = response["Item"]
+            s3_bucket = config.get("S3BucketARN")
+            jq_expression = config.get("JQExpression")
+
+            if not s3_bucket or not jq_expression:
+                return {
+                    "statusCode": 400,
+                    "body": json.dumps(f"Missing required config table fields."),
+                }
+        except (json.JSONDecodeError, KeyError) as e:
+            return {
+                "statusCode": 400,
+                "body": json.dumps(f"Invalid configuration: {str(e)}"),
+            }
 
         # Log the data and configuration for now
         print(f"Received data: {data}")
         print(
-            f"User config: S3 Bucket ARN = {s3_bucket}, JQ Expression = {jq_expression}"
+            f"Email: {email}\nS3 Bucket ARN = {s3_bucket}\nJQ Expression = {jq_expression}"
         )
 
         # Prepare payload for transformation lambda
@@ -65,3 +93,14 @@ def lambda_handler(event, context):
             "statusCode": 500,
             "body": json.dumps(f"Error fetching config: {str(e)}"),
         }
+
+
+def validate_api_key(api_key, email):
+    try:
+        response = api_key_table.get_item(Key={"Email": email})
+        if "Item" in response and response["Item"].get("APIKey") == api_key:
+            return True
+        return False
+    except Exception as e:
+        print(f"Error validating API key for email: {email}: {e}")
+        return False
